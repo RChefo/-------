91900509aaec4e7795974a39e2138a7ba74b29e9
RChefo <rchefocontact.com@gmail.com>
2026-04-28 10:41:56 +0300

    hi

--- bot.py
+++ bot.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
